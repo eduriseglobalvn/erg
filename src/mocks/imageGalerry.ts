fbe0cd4227dad41a69d1d296470172ac783e691a
Vương Trần <82163378+vuongtrann@users.noreply.github.com>
2025-12-24 13:17:32 +0700

    update AI

--- src/mocks/imageGalerry.ts
+++ src/mocks/imageGalerry.ts
@@ -5,3 +5,19 @@ export const REAL_IMAGES: string[] = [
     "https://images.unsplash.com/photo-1580582932707-520aed937b7b?q=80&w=800&auto=format&fit=crop",
     "https://images.unsplash.com/photo-1531545514256-b1400bc00f31?q=80&w=800&auto=format&fit=crop"
 ];
+
+export const AI_IMAGES: string[] = [
+    "/anhkhoahoc/AI/1.jpg",
+    "/anhkhoahoc/AI/2.jpg",
+    "/anhkhoahoc/AI/3.jpg",
+    "/anhkhoahoc/AI/4.jpg",
+    "/anhkhoahoc/AI/5.jpg",
+    "/anhkhoahoc/AI/6.jpg",
+    "/anhkhoahoc/AI/7.jpg",
+    "/anhkhoahoc/AI/8.jpg",
+    "/anhkhoahoc/AI/9.jpg",
+    "/anhkhoahoc/AI/10.jpg",
+    "/anhkhoahoc/AI/11.jpg",
+    "/anhkhoahoc/AI/12.jpg",
+
+]
